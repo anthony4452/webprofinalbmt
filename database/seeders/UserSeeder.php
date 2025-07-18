d5267161b17d4e7701118f97fa07684d383324bf
Anthony Morales <anthony.morales4452@utc.edu.ec>
2025-07-17 21:47:08 -0500

    xdxd

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,27 +1,30 @@
 <?php
 
 namespace Database\Seeders;
-
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::create([
-            'name' => 'Administrador',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('admin123'),
-            'role' => 'admin',
-        ]);
+        if (!User::where('email', 'admin@example.com')->exists()) {
+            User::create([
+                'name' => 'Administrador',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+            ]);
+        }
 
-        User::create([
-            'name' => 'Usuario Normal',
-            'email' => 'user@example.com',
-            'password' => Hash::make('user123'),
-            'role' => 'user',
-        ]);
+        if (!User::where('email', 'user@example.com')->exists()) {
+            User::create([
+                'name' => 'Usuario Normal',
+                'email' => 'user@example.com',
+                'password' => Hash::make('user123'),
+                'role' => 'user',
+            ]);
+        }
     }
 }
